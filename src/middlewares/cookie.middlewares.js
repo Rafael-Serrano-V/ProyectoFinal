a700e4cc1c9d983183b227d8b8be2d1a68aeb904
WardMore <rafaelmserranov@gmail.com>
2022-09-12 19:45:42 -0300

    Añade validación de token undefined

--- src/middlewares/cookie.middlewares.js
+++ src/middlewares/cookie.middlewares.js
@@ -1,7 +1,7 @@
 //Si la cookie no esta presente, se redirije a la página de registro, en caso de que exista se pasa al siguiente middleware
 const cookieRutaProtegida = (req, res, next)=> {
-    if(!req.cookies.moonToken){
-        return res.redirect('/registro');
+    if(typeof req.cookies.moonToken === 'undefined'){
+        return res.redirect('/login');
     }
     next();
 }
