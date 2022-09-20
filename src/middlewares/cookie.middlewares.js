9dc55e0daf0f77fdb171aa035a93d3619b83b4f7
WardMore <rafaelmserranov@gmail.com>
2022-09-19 21:39:03 -0300

    Añade validaciones

--- src/middlewares/cookie.middlewares.js
+++ src/middlewares/cookie.middlewares.js
@@ -1,7 +1,7 @@
 //Si la cookie no esta presente, se redirije a la página de registro, en caso de que exista se pasa al siguiente middleware
 const cookieRutaProtegida = (req, res, next)=> {
-    if(typeof req.cookies.moonToken === 'undefined'){
-        return res.redirect('/login');
+    if(typeof req.cookies.moonToken === 'undefined' || req.cookies.moonToken == null || req.cookies.moonToken == ""){
+        return res.redirect('/');
     }
     next();
 }
